11b7518797c6d171b9b98500de481cb3bd4528a9
Dzyniu23 <krzysztofrak93@gmail.com>
2021-10-25 10:52:38 +0200

    High Score stored in txt

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
-        self.high_score = 0
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 260)
         self.hideturtle()
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
